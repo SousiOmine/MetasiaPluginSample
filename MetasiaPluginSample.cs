648cc8a737f8983fac54c44cc3f00eb6eabe8aa5
Sousi Omine <110832262+SousiOmine@users.noreply.github.com>
2026-04-23 11:49:12 +0900

    ﾁｮｯﾄﾂｸﾙ

---
+++ MetasiaPluginSample.cs
@@ -0,0 +1,29 @@
+using Metasia.Editor.Abstractions.Hosting;
+using Metasia.Editor.Plugin;
+
+namespace MetasiaPluginSample;
+
+public class MetasiaPluginSample : IClipTypeProvider
+{
+    public string PluginIdentifier => "com.SousiOmine.MetasiaPluginSample";
+
+    public string PluginVersion => "1.0.0";
+
+    public string PluginName => "サンプルプラグイン";
+
+    public IEnumerable<IEditorPlugin.SupportEnvironment> SupportedEnvironments => [
+        IEditorPlugin.SupportEnvironment.Windows_x64,
+        IEditorPlugin.SupportEnvironment.MacOS_arm64,
+        IEditorPlugin.SupportEnvironment.Linux_x64
+    ];
+
+    public IEnumerable<Type> GetClipTypes()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Initialize(IEditorHostContext hostContext)
+    {
+        throw new NotImplementedException();
+    }
+}
